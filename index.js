796fbd52cc2e90d3e923b5d2aa4d6011e7b7280a
prems02 <161747244+prems02@users.noreply.github.com>
2025-08-03 15:01:50 +0530

    Update index.js
    updated dd

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ const userRouter = require("./routes/user.js");
 
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
-//const dbUrl = process.env.ATLASDB_URL;
+//const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
 .then(() =>{
@@ -39,7 +39,7 @@ main()
 });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 /*app.get("/" ,(req,res) =>{
